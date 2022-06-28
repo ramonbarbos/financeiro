40da4eea4d68335f7bc65e5a6f5c5b1f7928b8db
Ramon Barbosa <86085266+ramonbarbos@users.noreply.github.com>
2022-06-28 16:14:34 -0300

    delete

--- src/database/criarbd.js
+++
@@ -1,8 +0,0 @@
-import * as SQLite from 'expo-sqlite';
-
-// Conexão com o Banco de Dados do Sqlite 
-export const Criar = {
-  getConnection: () => SQLite.openDatabase("criar.db"),
-};
-
-//AVISO! Se quiser modificar o banco de dados, mude o nome e atualize o app no celular
